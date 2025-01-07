bcdf183b532fe13a839b57135501d47c049436e2
Can Burak Sofyalıoğlu <cbsofyalioglu@gmail.com>
2025-01-08 01:02:48 +0300

    users router was added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from .routers import users
 
 app = FastAPI()
 
+
+
+app.include_router(users.router)
 
 @app.get("/")
 async def read_root():
